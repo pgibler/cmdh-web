7b2210d982fabf0052d522d7d5695e71be2599f4
pgibler <paul.gibler@gmail.com>
2023-12-23 07:09:42 -0500

    Added official openai API + streaming

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,61 +1,52 @@
-import { NextResponse } from "next/server";
-import { createCompletion } from "./openai_api";
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
+import OpenAI from 'openai';
+import { validateApiKey } from '../validateApiKey';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC5f0QRTbWybRoa3Pr5ooXUECzea4toRGQ",
-  authDomain: "cmdh-ai.firebaseapp.com",
-  projectId: "cmdh-ai",
-  // other config properties
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const openai = new OpenAI();
 
-// After initializing Firebase
-const firestore = firebase.firestore();
-
-// Optional: If using Firestore emulator
-if (process.env.NODE_ENV === 'development') {
-  firestore.useEmulator('localhost', 8080); // Replace with your Firestore emulator port
-}
+export const runtime = 'edge';
 
 export async function POST(request: Request) {
-  try {
-    const { prompt, system, apiKey } = await request.json();
-    if (!apiKey) throw new Error('No API key provided');
+  const { apiKey, model, prompt, system } = await request.json();
 
-    // Verify the API key
-    // Assume we have a function to validate API key against Firestore
-    const isValidApiKey = await validateApiKey(apiKey);
-    if (!isValidApiKey) throw new Error('Invalid API key');
-    const text = await createCompletion(prompt, system);
+  if (!apiKey) throw new Error('No API key provided');
 
-    return new NextResponse(JSON.stringify({ text }));
-  } catch (error) {
-    let errorMessage = 'Unknown error occurred';
+  // Verify the API key
+  // Assume we have a function to validate API key against Firestore
+  const isValidApiKey = await validateApiKey(apiKey);
+  if (!isValidApiKey) throw new Error('Invalid API key');
 
-    if (error instanceof Error) {
-      errorMessage = error.message;
-    }
+  const stream = await buildStream(model, prompt, system);
 
-    return new NextResponse(JSON.stringify({ error: errorMessage }), {
-      status: 401,
-    });
-  }
+  // Return the complete response data
+  return new Response(stream);
+};
+
+async function buildStream(model: string, prompt: string, system: string) {
+  return new ReadableStream({
+    async start(controller) {
+      const encoder = new TextEncoder();
+      const stream = await openai.chat.completions.create({
+        model: model,
+        messages: [
+          { role: 'system', content: system },
+          { role: 'user', content: prompt }
+        ],
+        stream: true,
+      });
+
+      try {
+        // Collecting data from the stream
+        for await (const chunk of stream) {
+          // Assuming chunk is a string or can be converted to string
+          const content = chunk.choices[0].delta.content
+          if (content) {
+            controller.enqueue(encoder.encode(content));
+          }
+        }
+        controller.close();
+      } catch (e) {
+        controller.error(e);
+      }
+    },
+  });
 }
-
-async function validateApiKey(apiKey: string) {
-  const usersRef = firestore.collection('users');
-  const snapshot = await usersRef.where('apiKey', '==', apiKey).limit(1).get();
-
-  if (snapshot.empty) {
-    return false; // API key not found
-  }
-
-  // Additional checks can be added here (e.g., checking if the user's account is active)
-
-  return true; // API key is valid
-}
-
-
